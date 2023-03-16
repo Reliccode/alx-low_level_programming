e6a275fa53cdf648c607c18dcecdbdd04501c514
Reliccode <bryanmaritim@gmail.com>
2023-03-16 22:25:53 +0000

    made changes to code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints size of various types
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("The size of char is %lu bytes.\n", sizeof(char));
+	printf("The size of short is %lu bytes.\n", sizeof(short));
+	printf("The size of int is %lu bytes.\n" sizeof(int));
+	printf("The size of long is %lu bytes.\n"sizeof(long));
+	printf("The size of long long is %lu bytes.\n", sizeof(long long));
+	printf("The size of float is %lu bytes.\n", sizeof(float));
+	printf("The size of double is %lu bytes.\n", sizeof(double));
+	printf("The size of long double is %lu bytes.\n", sizeof(long double));
+    
+    return (0);
+}
+
