14dd42ff5b82b8081197174e930de74d84f89822
Reliccode <bryanmaritim@gmail.com>
2023-03-16 20:00:07 +0000

    created file 4-puts.c with C command to print line using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * parameter: Program prints a line using puts function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*printing using puts*/
+	char Mystr[] = "\"Programming is like building a multilingual puzzle"
+
+	puts(Mystr); /*Writing the string to stdout*/
+
+	return (0);
+}
