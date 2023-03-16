bd98a9da81641b8eb4dc285f0dbb5d54a2348847
Reliccode <bryanmaritim@gmail.com>
2023-03-16 21:53:41 +0000

    created file 5-printf.c to print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main-Entry point
+ * The program uses printf to print
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	/*printing using printf*/
+	char Exctly[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(Exctly \n); /*Writing the string to the stdout*/
+
+	return (0);
+}
