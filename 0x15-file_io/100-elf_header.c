3e23283e61f40d5081b6b13262853099f055365e
Reliccode <bryanmaritim@gmail.com>
2023-05-12 06:09:01 +0000

    Tried new code

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,44 +11,45 @@
  * @ptr: 
  * Return: no return.
  */
-void print_addr(char *ptr) {
-    int i;
-    int begin;
-    char sys;
+void print_addr(char *ptr)
+{
+	int i;
+	int begin;
+	char sys;
 
-    printf("  Entry point address:               0x");
+	printf("  Entry point address:               0x");
 
-    sys = ptr[4] + '0';
+	sys = ptr[4] + '0';
+	if (sys == '1')
+	{
+		begin = 26;
+		printf("80");
+		for (i = begin; i >= 22; i--)
+		{
+			if (ptr[i] > 0)
+				printf("%x", ptr[i]);
+			else if (ptr[i] < 0)
+				printf("%x", 256 + ptr[i]);
+		}
+		if (ptr[7] == 6)
+			printf("00");
+	}
 
-    if (sys == '1') {
-        begin = 26;
-        printf("80");
+	if (sys == '2')
+	{
+		begin = 26;
+		for (i = begin; i > 23; i--)
+		{
+			if (ptr[i] >= 0)
+				printf("%02x", ptr[i]);
 
-        for (i = begin; i >= 22; i--) {
-            if (ptr[i] > 0)
-                printf("%x", ptr[i]);
-            else if (ptr[i] < 0)
-                printf("%x", 256 + ptr[i]);
-        }
+			else if (ptr[i] < 0)
+				printf("%02x", 256 + ptr[i]);
 
-        if (ptr[7] == 6)
-            printf("00");
-    }
-
-    if (sys == '2') {
-        begin = 26;
-
-        for (i = begin; i > 23; i--) {
-            if (ptr[i] >= 0)
-                printf("%02x", ptr[i]);
-            else if (ptr[i] < 0)
-                printf("%02x", 256 + ptr[i]);
-        }
-    }
-
-    printf("\n");
+		}
+	}
+	printf("\n");
 }
-
 
 /**
  * print_type - it prints type
@@ -242,4 +243,4 @@ int main(int argc, char *argv[])
 	close(fd);
 
 	return (0);
-}                                                                                
+}
